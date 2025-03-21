9576cb8874f0b5217d698daf93b4659b94987d96
SolRollaz <customai.nz@gmail.com>
2025-03-21 19:48:31 +1300

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-import SystemConfig from './systemConfig.js';
-
-const config = new SystemConfig();
-
-console.log("Mongo URI:", config.getMongoUri());
-console.log("Mongo Database Name:", config.getMongoDbName());

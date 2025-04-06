86da2b4381bce3f072c5b5b6ce8b37a376b23711
SolRollaz <customai.nz@gmail.com>
2025-04-06 15:55:35 +1200

    Create gameWalletsSchema.js

---
+++ Schema/gameWalletsSchema.js
@@ -0,0 +1,38 @@
+// File: /Schema/gameWalletsSchema.js
+
+const mongoose = require('mongoose');
+
+const walletTypes = ['Rewards', 'PrizePools'];
+
+const gameWalletsSchema = new mongoose.Schema({
+  gameKey: {
+    type: String,
+    required: true,
+    index: true
+  },
+  type: {
+    type: String,
+    enum: walletTypes,
+    required: true
+  },
+  network: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  challengeId: {
+    type: String,
+    required: function () {
+      return this.type === 'PrizePools';
+    }
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('GameWallets', gameWalletsSchema);
